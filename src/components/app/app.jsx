97357809cba47d8c5329740bf4af53402e31dbf0
ipolekhin <ipolekhin@yadnex.ru>
2020-06-11 14:57:16 +0300

    1.3 Установил React и создал первые компоненты

---
+++ src/components/app/app.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+
+const App = () => {
+  return (
+    <WelcomeScreen />
+  );
+};
+
+export default App;
+
